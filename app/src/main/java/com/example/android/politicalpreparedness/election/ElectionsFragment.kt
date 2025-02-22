3fa00d70a9035eb1f9f087ee545e4082c46c96ab
macky-boi <125427131+macky-boi@users.noreply.github.com>
2025-02-22 13:22:57 +0800

    launch to elections navigation

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 
 class ElectionsFragment: Fragment() {
 
     // TODO: Declare ViewModel
+
+    private lateinit var binding: FragmentElectionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,13 +23,19 @@ class ElectionsFragment: Fragment() {
         // TODO: Add ViewModel values and create ViewModel
 
         // TODO: Add binding values
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_election, container,false)
 
         // TODO: Link elections to voter info
 
         // TODO: Initiate recycler adapters
 
         // TODO: Populate recycler adapters
-        return null
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     // TODO: Refresh adapters when fragment loads
